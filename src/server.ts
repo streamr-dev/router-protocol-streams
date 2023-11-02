06323405bc61116563423b6cc5a533f542d83838
Simon <phoenixbae3@gmail.com>
2023-11-02 11:16:58 +0200

    refactor: load env config before server creation

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { startRouterService } from './services/router-service';
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-dotenv.config();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
