9b76f401702d964dcf6246858fd0410913d7d954
Simon <phoenixbae3@gmail.com>
2023-11-02 11:18:44 +0200

    fix: rename port to PORT

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { startRouterService } from './services/router-service';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
